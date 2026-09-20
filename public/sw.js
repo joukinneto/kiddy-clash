cc179f93b07d85358ae4dfdc714cfe6d3e125da6
Joaquim Neto <joaquimnetoengcivil@gmail.com>
2026-09-20 09:58:23 -0400

    feat(pwa): add sw.js

---
+++ public/sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'kiddy-clash-shell-v1'
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './app-icon.svg',
+  './assets/ui/kiddy-clash-logo.svg',
+  './assets/characters/leo.svg',
+  './assets/characters/pandy.svg',
+  './assets/characters/bibi.svg',
+  './assets/characters/foxy.svg',
+  './assets/characters/max.svg',
+  './assets/characters/mimi.svg',
+  './assets/characters/dino.svg',
+  './assets/characters/b01.svg'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)),
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key)),
+      ),
+    ),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+
+  const requestUrl = new URL(event.request.url)
+  if (requestUrl.origin !== self.location.origin) return
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy))
+          return response
+        })
+        .catch(() => caches.match('./index.html')),
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached
+
+      return fetch(event.request).then((response) => {
+        if (response.ok) {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+        }
+        return response
+      })
+    }),
+  )
+})

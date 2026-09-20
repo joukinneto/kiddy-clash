2220abd8785be8605fda7664e7532e3aeb1d37b4
Joaquim Neto <joaquimnetoengcivil@gmail.com>
2026-09-20 09:15:52 -0400

    chore: configure Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.jkddtech.kiddyclash',
+  appName: 'Kiddy Clash',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+}
+
+export default config
